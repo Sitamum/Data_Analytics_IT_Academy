c8ef1037838dee4b73913ae67b377872088bb2f4
Sitamum <105667947+Sitamum@users.noreply.github.com>
2024-10-29 13:30:02 +0100

    listo para correccion

--- Sprint3/Sprint3_Nivel3.sql
+++ Sprint3/Sprint3_Nivel3.sql
@@ -7,7 +7,7 @@
 show create table company
 -- nos muestra este resultado
 -- 'company', 'CREATE TABLE `company` (\n  `id` varchar(15) NOT NULL,\n  `company_name` varchar(255) DEFAULT NULL,\n  `phone` varchar(15) DEFAULT NULL,\n  `email` varchar(100) DEFAULT NULL,\n  `country` varchar(100) DEFAULT NULL,\n  `website` varchar(255) DEFAULT NULL,\n  PRIMARY KEY (`id`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci'
-ALTER TABLE transaction
+ALTER TABLE company
 DROP COLUMN website;
 -- comprobamos 
 DESCRIBE company;
@@ -20,7 +20,9 @@ DESCRIBE Credit_card;
 -- mismo proceso tabla 'user' , 
 DESCRIBE user;
 
+
 -- tenemos que cambiarle el nombre a la tabla por 'data_user' y
+RENAME TABLE user to Data_user;
 -- cambiar el nombre de columna 'email' por 'personal_email'
 ALTER TABLE Data_user
 RENAME COLUMN email TO personal_email ;
